802c4f8735e179a380b3a1b3ff391681ba68083e
SecureStaking.io <73739461+securestaking@users.noreply.github.com>
2020-11-09 11:51:52 +0100

    Update start.sh
    Target host by default to 127.0.0.1

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ docker rm cardano-cncli
 docker run -d \
 	-v $PWD/sqlite:/srv/cardano/cardano-cli/storage \
 	--name cardano-cncli \
-	-e TARGET_HOST=192.168.1.118 \
+	-e TARGET_HOST=127.0.0.1 \
 	-e TARGET_PORT=3000 \
 	-e DB_LOCATION=storage/sqlite.db \
 	-e RUST_BACKTRACE=1 \
